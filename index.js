d59433d7f43c8d6293005f99a40157c7d88cdae8
Sam007k <msameer007k@gmail.com>
2021-03-21 20:02:54 +0530

    fixed tap keyboard

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function displayWord() {
 
 
 // Keydown letter press
-window.addEventListener('keydown', e => {
+window.addEventListener('pointerdown', e => {
     // console.log(e.keyCode);
     if (e.keyCode >= 65 && e.keyCode <= 90) {
       const letter = e.key;
